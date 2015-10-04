8ee85fbf0271b83c0dbc01c328296d7e360f04d9
Martin Kostov <centrino2duo@gmail.com>
2015-10-04 13:44:31 +0300

    improved population hire

--- mKIOAutomizer/iov5/iov5Population.js
+++ mKIOAutomizer/iov5/iov5Population.js
@@ -1,15 +1,23 @@
-Executer.register({
-    getTimeout: function() { return 2 * 60 * 1000; },
-    exec: function(config) {
-        var hire = function(resourceID, timeout) {
-            setTimeout(function() {
-                xajax.request({ xjxfun: 'fastHireWorkers' }, { parameters: [123199, resourceID, 'AllProv', 2, { 'hireAll': 1 }] });
-            }, timeout);
-        };
-        if (config.isAutoHireEnabled) {
-            hire(config.autoHireSettings[0], 0);
-            hire(config.autoHireSettings[1], 1000);
-            hire(config.autoHireSettings[2], 2000);
+(function () {
+    var hire = function (resourceID, cb) {
+        Executer.post({
+            n: "fastHireWorkers",
+            p: ["N123199", "S" + resourceID, "SAllProv", "N2", "<xjxobj><e><k>hireAll</k><v>N" + 1 + "</v></e></xjxobj>"],
+            f: cb
+        });
+    };
+
+    Executer.register({
+        getTimeout: function() { return 2 * 60 * 1000; },
+        exec: function(config) {
+            if (config.isAutoHireEnabled) {
+                hire(config.autoHireSettings[0], function () {
+                    hire(config.autoHireSettings[1], function () {
+                        hire(config.autoHireSettings[2])
+                    });
+                });
+            }
         }
-    }
-});
+    });
+
+}());
